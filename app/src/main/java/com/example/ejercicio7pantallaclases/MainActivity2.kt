ab5141f2867fe94f3a251593e3ce51ba7832c882
Pablo <pabloalonsogarc@gmailc.om>
2023-01-24 18:47:27 +0100

    RAzas terminadas

--- app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity2.kt
+++ app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity2.kt
@@ -1,11 +1,41 @@
 package com.example.ejercicio7pantallaclases
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio7pantallaclases.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding=ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            binding.imageView.setImageResource(R.drawable.humano)
+        }
+        binding.button2.setOnClickListener{
+            binding.imageView.setImageResource(R.drawable.elfo)
+        }
+        binding.button3.setOnClickListener{
+            binding.imageView.setImageResource(R.drawable.ogro)
+        }
+        binding.button4.setOnClickListener{
+            binding.imageView.setImageResource(R.drawable.gnomo)
+        }
+        binding.button5.setOnClickListener{
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
     }
-}
+     }
+
+
+
+
