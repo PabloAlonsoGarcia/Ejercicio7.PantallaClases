ce415537f65a9926b893c51b3bb6b7f6d1da5b1f
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2023-01-24 12:29:33 +0100

    Creado

--- app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.ejercicio7pantallaclases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio7pantallaclases.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            binding.imageView.setImageResource(R.drawable.arquero)
+        }
+
+
     }
+
+
+
+
+
 }
