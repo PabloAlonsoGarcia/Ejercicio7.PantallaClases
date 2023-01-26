e189722879d8cc4963060470c0d96b55620a3c72
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2023-01-26 14:08:48 +0100

    Pantlla 03 medio creada

--- app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity3.kt
+++ app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity3.kt
@@ -2,10 +2,23 @@ package com.example.ejercicio7pantallaclases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio7pantallaclases.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        binding=ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.editTextTextPersonName2.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                binding.editTextTextPersonName2.setText("")
+            }
+        }
+
+
     }
 }
