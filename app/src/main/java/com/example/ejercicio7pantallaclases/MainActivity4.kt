a9a6b3eb1ca6f38a4d0bfdf9d249577e68d02c8a
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2023-01-31 11:01:13 +0100

    Pantalla paisaje terminada

--- app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity4.kt
+++ app/src/main/java/com/example/ejercicio7pantallaclases/MainActivity4.kt
@@ -1,11 +1,47 @@
 package com.example.ejercicio7pantallaclases
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio7pantallaclases.databinding.ActivityMain4Binding
 
 class MainActivity4 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain4Binding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
+        binding=ActivityMain4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.imageButton2.setOnClickListener(){
+            aleatorio()
+        }
+
+
+
     }
+    @SuppressLint("SetTextI18n")
+    fun aleatorio(){
+        var aux=0
+
+        aux=(1..4).random()
+
+        when(aux){
+            1->{
+                binding.texto2.text="Has encontrado un objeto"
+
+            }
+            2->{
+                binding.texto2.text="Has encontrado una ciudad"
+            }
+            3->{
+                binding.texto2.text="Has encontrado un mercader"
+            }
+            4->{
+                binding.texto2.text="Has encontrado un enemigo"
+            }
+        }
+    }
+
 }
